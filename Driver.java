7e861b6be14f14a42da61b51f1152349c45ba2ff
AlexanderZhang(CodingRookieA) <zhangalexander6@gmail.com>
2024-09-29 23:39:43 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -11,13 +11,8 @@ public class Driver {
 		double [] c1 = {6,1,1,5};
 		int[] c1_exp = {0,1,2,3};
 		Polynomial p1 = new Polynomial(c1,c1_exp);
-		for(int i = 0; i<4;i++){
-			System.out.println("coefficient"+ c1[i] + "exponential" + c1_exp[i]);
-		}
 		File poly = new File("poly.txt");
-		double[] c2 = {3,1,2,4,2};
-		int[] c2_exp = {0,1,2,3,5};
-		Polynomial p2 = new Polynomial(c2,c2_exp);
+		Polynomial p2 = new Polynomial(poly);
 		
 		Polynomial s = p1.add(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
